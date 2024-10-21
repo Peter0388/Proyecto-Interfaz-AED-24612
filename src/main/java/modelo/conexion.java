9f2399ec6fb470137f7e31708e1c8372d262fb9d
Alucard <Alucard@DESKTOP-HTSES8T>
2024-10-21 03:42:19 -0500

    Agregar conexion Prueba exitosa

---
+++ src/main/java/modelo/conexion.java
@@ -0,0 +1,30 @@
+
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class conexion {
+    
+    Connection conectar = null;
+    
+    String usuario = "root";
+    String contrasenia = "75985029";
+    String bd = "tiendabd";
+    String ip = "localhost";
+    String puerto = "3306";
+    
+    String cadena = "jdbc:mysql://"+ip+":"+puerto+"/"+bd;
+    
+    public Connection establecerConexion(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conectar = DriverManager.getConnection(cadena, usuario, contrasenia);
+            JOptionPane.showMessageDialog(null, "Conexion exitosa");
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Error al conectarse, error:"+e.toString());
+        }
+       return conectar;
+    }
+}
--- src/main/java/vista/main.java
+++ src/main/java/vista/main.java
@@ -1,10 +1,13 @@
 
 package vista;
 
+import modelo.conexion;
+
 public class main {
 
     public static void main(String[] args) {
-        
+        conexion objetoConexion = new conexion();
+        objetoConexion.establecerConexion();
     }
     
 }
